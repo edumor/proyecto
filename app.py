0ff7b8b71edb8d221f1fc01ddd6da5f4a8a017a7
Eduardo Moreno <163068533+edumor@users.noreply.github.com>
2025-05-21 21:59:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.secret_key = 'your_secret_key'  # Necesario para usar flash
 # Configuración de la base de datos
 db_config = {
     'user': 'edumor',
-    'password': 'Riva8055',
+    'password': '********',
     'host': 'edumor.mysql.pythonanywhere-services.com',
     'database': 'edumor$default'
 }
